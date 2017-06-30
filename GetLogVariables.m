bf461006653f6d62712fdb1ca628d9caebea56b5
Shuuji Kajita <kajita@aist.go.jp>
2017-06-30 18:51:22 +0900

    messages to help users

--- GetLogVariables.m
+++ GetLogVariables.m
@@ -3,7 +3,24 @@
 if ~exist('data_name')
   data_name = input('log file name: ','s');
 end
-labels = GetLogLabels(data_name);
+if exist(data_name,'file')
+  labels = GetLogLabels(data_name);
+else
+  fprintf('ERROR: %s does not exist\n',data_name);
+  clear data_name
+  return;
+end
+
+fprintf('VARIABLES: ');
+for n=1:length(labels)
+  if labels(n).num == 1
+    fprintf('%s ',labels(n).string);
+  else
+    fprintf('%s[%d] ',labels(n).string,labels(n).num);
+  end
+end
+fprintf('\n');
+
 data = load(data_name);
 
 col = 1;
@@ -13,13 +30,13 @@ for n=1:length(labels)
   if labels(n).num == 1
     eval( [labels(n).string,'=data(:,', num2str(col),  ');'] );   % eg. time
   elseif labels(n).num > 1
-    eval( [labels(n).string,'=data(:,', num2str(col),':', num2str(next_col-1), ');'] );  % eg. com[1] com[2] 
+    eval( [labels(n).string,'=data(:,', num2str(col),':', num2str(next_col-1), ');'] );  % eg. com[1] com[2]
   else
     fprintf('Error: string= %s, num = %d\n',labels(n).string,labels(n).num); 
   end
   col = next_col;
 end 
-clear n col next_col
+clear n col next_col data_name
 
 %tsize = length(time);
 %ToDeg = 180/pi;
--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 Matlab/Octave utility for log data analysis
 =======
 
+How to use
+-----
+
 ```
 >> clear
 >> type testdata.log
+% time dataX[1] dataX[2]
+0 0 1
+0.01 0.00999983 0.99995
+0.02 0.0199987 0.9998
+0.03 0.0299955 0.99955
+0.04 0.0399893 0.9992
+0.05 0.0499792 0.99875
+0.06 0.059964 0.998201
+0.07 0.0699428 0.997551
+-- less -- (f)orward, (b)ack, (q)uit
 
 >> GetLogVariables
 log file name: testdata.log
->> who
->> plot(time,x,time,y)
+VARIABLES: time dataX[2] 
+
+>> plot(time,dataX)
 ```
